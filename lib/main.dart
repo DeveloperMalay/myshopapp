bbbcad7396783c1f42e22a2c0c419243b1433e42
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-04-11 21:57:52 +0530

    making some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerce_app/core/router/router.dart';
-import 'package:ecommerce_app/home/presentation/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
